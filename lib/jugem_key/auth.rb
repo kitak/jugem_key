e0fe1d2235f63e021123ee06fbd5599995edeed9
Keisuke KITA <kei.kita2501@gmail.com>
2013-01-24 21:33:13 +0900

    fake implement token

--- lib/jugem_key/auth.rb
+++ lib/jugem_key/auth.rb
@@ -1,6 +1,9 @@
 # coding: utf-8
-require 'openssl'
 require 'cgi'
+require 'feed-normalizer'
+require 'net/http'
+require 'openssl'
+require 'time'
 
 module JugemKey
   class Auth
@@ -13,11 +16,17 @@ module JugemKey
     end
 
     def uri_to_login(params)
+      sig = api_sig({
+        api_key: @api_key,
+        callback_url: params[:callback_uri],
+        perms: "read"      
+      })
+
       query = {
         mode:    "auth_issue_frob",
         api_key: @api_key,
         perms:   "read",
-        api_sig: api_sig(params)
+        api_sig: sig
       }.merge(params).map { |k, v|
         k.to_s + "=" + CGI.escape(v)
       }.join('&')
@@ -25,20 +34,64 @@ module JugemKey
       URI.join(JUGEMKEY_URL, '?', query)
     end
 
+    def token(frob)
+      sig = {
+        api_key: @api_key,
+        created: Time.now.iso8601, 
+        frob:    frob
+      } 
+      atom = fetch_atom('http://api.jugemkey.jp/api/auth/token', {
+        "X-JUGEMKEY-API-CREATED": Time.now.iso8601,
+        "X-JUGEMKEY-API-KEY":     @api_key,
+        "X-JUGEMKEY-API-FROB":    frob,
+        "X-JUGEMKEY-API-SIG":     sig
+      })
+
+      require 'pp'
+      pp atom
+      pp FeedNormalizer::FeedNormalizer.parse(atom)
+      'hoge'
+    end
+
+    def user(token)
+      sig = {
+        api_key: @api_key,
+        created: Time.now.iso8601, 
+        token:   token 
+      } 
+      fetch_atom('http://api.jugemkey.jp/api/auth/user', {
+        "X-JUGEMKEY-API-CREATED": Time.now.iso8601,
+        "X-JUGEMKEY-API-KEY":     @api_key,
+        "X-JUGEMKEY-API-TOKEN":   token,
+        "X-JUGEMKEY-API-SIG":     sig
+      })
+
+      require 'pp'
+      pp atom
+      pp FeedNormalizer::FeedNormalizer.parse(atom)
+      'hoge'
+    end
+
     private
     def api_sig(params)
-      value = "#{@api_key}#{params[:callback_uri]}read"
+      value = params.map{|k, v| v}.join('')
       OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, @secret, value)
     end
-  end
-end
 
-if __FILE__ == $0
-  api = JugemKey::Auth.new({
-    api_key: '40025ab515df245d2483d758ca9d0680',
-    secret:  '1d4c74a7cc19aeb1'
-  })
-  puts api.uri_to_login({
-    callback_url: 'http://jugemkey.jp/' 
-  })
+    def fetch_atom(url, header)
+      Net::HTTP.version_1_2
+      url = URI.parse(url)
+
+      Net::HTTP.start(url.host, url.port) do |http|
+        res = http.get(url.path, header)
+
+        if res.class == Net::HTTPOK
+          return res.body
+        else
+          raise res.message
+        end
+      end
+    end
+
+  end
 end
